0c5f4a4f3dd568c684bebbdf4660ad1f902020e1
Muhammad Anas <anassoomro23@gmail.com>
2025-02-07 01:52:02 +0500

    header responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,37 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { decode } from "html-entities";
+import { RxHamburgerMenu } from "react-icons/rx";
 
-export default function header() {
+const links = [
+  { title: "Home", link: "#About" },
+  { title: "Projects", link: "#Project" },
+  { title: "Contact", link: "#Contact" },
+];
+const navlinkStyle = "transition-all duration-30 cursor-pointer";
+
+export default function Header() {
+  const [isHamburgerOpen, setIsHamBurgerOpen] = useState(false);
+
   return (
-    <>
-      <header className="  custom-bg flex justify-between  items-center p-4">
-        <h1 className=" ">
-          <a href="/" className=" text-3xl   ">
-            {decode("&lt")}{" "}
-            <span className="  specific-color allura-regular text-4xl underline-duration-1000  transition-all duration-800 hover:underline hover:decoration-2">
-              M.Anas
-            </span>{" "}
-            {decode("&gt")}
-          </a>
-        </h1>
-        <ul className="  flex  text-lg justify-evenly gap-10 mr-14">
-          <li className=" hover:underline relative inline-block overflow-hidden ">
-            <a
-              href="#"
-              class=" hover:underline decoration-[#3fc1a3] decoration-2 jtransition-all duration-500"
-            >
-              About
-            </a>
+    <header className="custom-bg flex justify-between items-center w-dvw p-4">
+      <h1>
+        <a href="/" className="text-3xl">
+          {decode("&lt")}{" "}
+          <span className="specific-color allura-regular text-4xl underline-duration-1000 transition-all duration-800 hover:underline hover:decoration-2">
+            M.Anas
+          </span>{" "}
+          {decode("&gt")}
+        </a>
+      </h1>
+
+      <div
+        className="text-3xl  cursor-pointer "
+        onClick={() => setIsHamBurgerOpen(!isHamburgerOpen)}
+      > <span className="text-3xl mobile sm:hidden  ">
+        <RxHamburgerMenu />
+      </span>
+      </div>
+
+      <ul
+        className={`flex  text-lg justify-evenly gap-10 mr-14 ${
+          isHamburgerOpen ? "flex-col absolute top-full left-0 w-full bg-white" : "flex-row"
+        }`}
+      >
+        {links.map((item, i) => (
+          <li  className={`navlinkStyle hover:underline hover:decoration-[#3fc1a3] decoration-2 `} key={i}>
+            <a href={item.link}>{item.title}</a>
           </li>
-          <li>
-            <a  className="hover:underline decoration-[#3fc1a3] decoration-2 jtransition-all duration-500" href="">Projects</a>
-          </li>
-          <li>
-            {" "}
-            <a className="hover:underline decoration-[#3fc1a3] decoration-2 jtransition-all duration-500" href="">Contact</a>
-          </li>
-        </ul>
-      </header>
-    </>
+        ))}
+      </ul>
+    </header>
   );
 }
