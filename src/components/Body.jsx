f33e2c010ac3f04f2beae0c742e3af45d53dece3
Muhammad Anas <anassoomro23@gmail.com>
2025-02-13 10:10:15 +0500

    working on body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function Body() {
   return (
     <>
-    <div className=" flex  h-screen">
-    </div>
-      </>
-  )
+      <div className="bg-white w-full h-screen flex justify-center">
+        <div className="shadow-2xl bg-color w-[90%] p-6">
+          <h1 className="  text-white text-2xl">
+            Hi, My self
+            <br />
+            <span className=" block text-4xl specific-color font-thin">
+              Muhammad Anas
+            </span>
+            <br />
+            <span className="block text-3xl font-Arial text-color ">
+              I build sleek and interactive web experiences.
+            </span> 
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
