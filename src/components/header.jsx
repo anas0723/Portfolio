426ee3f84c6e2b61b2a4708f4fb3701d5a6a6587
Muhammad Anas <anassoomro23@gmail.com>
2025-02-25 02:21:30 +0500

    add resume and work sam issue

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,7 @@ const links = [
   { title: "Home", link: "#About" },
   { title: "Projects", link: "#Project" },
   { title: "Contact", link: "#Contact" },
+  // { title: "Resume", link: "#Resume"}
 ];
 
 export default function Header() {
@@ -14,9 +15,9 @@ export default function Header() {
   return (
     <header className="custom-bg flex justify-between items-center w-full p-4 relative">
       <h1>
-        <a href="/" className="text-3xl">
+        <a href="/" className="text-3xl text-right relative group">
           {decode("&lt")}{" "}
-          <span className="specific-color allura-regular text-4xl underline-duration-1000 transition-all duration-800 hover:underline hover:decoration-2">
+          <span className="specific-color allura-regular text-4xl relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#3fc1a3] after:transition-all after:duration-300 after:ease-in-out group-hover:after:w-full">
             M.Anas
           </span>{" "}
           {decode("&gt")}
@@ -34,26 +35,46 @@ export default function Header() {
       {/* Desktop Menu (Hidden on Small Screens) */}
       <ul className="hidden sm:flex text-lg justify-evenly gap-10 mr-14">
         {links.map((item, i) => (
-          <li
-            className="navlinkStyle hover:underline hover:decoration-[#3fc1a3] decoration-2"
-            key={i}
-          >
-            <a href={item.link}>{item.title}</a>
+          <li className="navlinkStyle py-2 text-right relative group" key={i}>
+            <a
+              className="relative text-white after:content-[''] after:absolute after:right-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#3fc1a3] after:transition-all after:duration-300 after:ease-in-out group-hover:after:w-full after:left-0"
+              href={item.link}
+            >
+              {item.title}
+            </a>
           </li>
         ))}
+        <li className="w-full flex justify-end ">
+          <a
+            className="rounded-2xl text-white px-4  py-2 border-2 border-transparent hover:border-sky-500 shadow-none hover:shadow-lg hover:shadow-sky-500 transition-all duration-300"
+            href="#resume"  
+          >
+            Resume
+          </a>
+        </li>
       </ul>
 
       {/* Mobile Menu (Visible when Hamburger is Open) */}
       {isHamburgerOpen && (
         <ul className="mobile-screen flex flex-col items-end absolute top-full right-0 w-full bg-[#0a192f] py-4 px-6 sm:hidden">
           {links.map((item, i) => (
-            <li
-              className="navlinkStyle py-2 text-right hover:underline hover:decoration-[#3fc1a3] decoration-2"
-              key={i}
-            >
-              <a href={item.link}>{item.title}</a>
+            <li className="navlinkStyle py-2 text-right relative group" key={i}>
+              <a
+                href={item.link}
+                className="relative text-white after:content-[''] after:absolute after:right-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#3fc1a3] after:transition-all after:duration-300 after:ease-in-out group-hover:after:w-full after:left-0"
+              >
+                {item.title}
+              </a>
             </li>
           ))}
+          <li className="w-full flex justify-end mt-2">
+            <a
+              className="rounded-2xl text-white px-4 py-2 border-2 border-transparent hover:border-amber-500 shadow-none hover:shadow-lg hover:shadow-amber-500 transition-all duration-300"
+              href="/resume"
+            >
+              Resume
+            </a>
+          </li>
         </ul>
       )}
     </header>
