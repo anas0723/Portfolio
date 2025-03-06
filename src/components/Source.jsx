90af639abca1a1c16b5adc7fcf1a6bb89bd94c17
Muhammad Anas <anassoomro23@gmail.com>
2025-03-06 23:48:53 +0500

    add .

--- src/components/Source.jsx
+++ src/components/Source.jsx
@@ -4,7 +4,7 @@ import MetaBalls from "./MetaBalls";
 const Source = () => {
 
   return (
-    <div className="fixed inset-0 overflow-hidden ">
+    <div className="fixed inset-0 overflow-hidden pointer-events-none ">
       <MetaBalls
        
         color="#3fc1a3"
