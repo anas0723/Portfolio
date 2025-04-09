4a78ce102212822c806af0c5dec417418e1bd6e2
Muhammad Anas <anassoomro23@gmail.com>
2025-04-10 04:45:33 +0500

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./index.css";
-import Body from "./components /Body.jsx";
-import Header from "./components /Header.jsx";
-import Cursor from "./components /Cursor.jsx";
+import Body from "./components/Body.jsx";
+import Header from "./components/Header.jsx";
+import Cursor from "./components/Cursor.jsx";
 
 export default function App() {
   return (
